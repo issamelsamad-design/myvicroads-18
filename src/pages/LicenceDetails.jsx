25afd3cd8be07ff60287af1dbea12847a5b792a5
Issam Elsamad <issamelsamad@Issams-MacBook-Air.local>
2026-06-13 14:16:24 +1000

    Increase diagonal line opacity on licence green background

--- src/pages/LicenceDetails.jsx
+++ src/pages/LicenceDetails.jsx
@@ -233,7 +233,7 @@ export default function LicenceDetails() {
         <svg className="absolute inset-0 w-full h-full" style={{ zIndex: 0, pointerEvents: 'none' }} xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice">
           <defs>
             <pattern id="diaglines" width="6" height="6" patternUnits="userSpaceOnUse" patternTransform="rotate(45)">
-              <line x1="0" y1="0" x2="0" y2="6" stroke="rgba(255,255,255,0.22)" strokeWidth="0.6"/>
+              <line x1="0" y1="0" x2="0" y2="6" stroke="rgba(255,255,255,0.34)" strokeWidth="0.6"/>
             </pattern>
           </defs>
           <rect width="100%" height="100%" fill="url(#diaglines)"/>
